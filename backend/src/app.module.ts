60b6f7d2dd4c36de12cd4011ee69e8d0ed2498e2
keodanic <victordanoelsc14@gmail.com>
2025-09-08 05:30:59 -0300

    enchment:Comandos e blibiotecas do package.json adicionados

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersService } from './modules/users/users.service';
+import { UsersController } from './modules/users/users.controller';
+import { UsersModule } from './modules/users/users.module';
+import { TypeUsersService } from './modules/type-users/type-users.service';
+import { TypeUsersController } from './modules/type-users/type-users.controller';
+import { TypeUsersModule } from './modules/type-users/type-users.module';
+import { PrismaModule } from './database/prisma.module';
+import { CaslModule } from './casl/casl.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, TypeUsersModule,CaslModule,PrismaModule,AuthModule],
+  controllers: [AppController, UsersController, TypeUsersController],
+  providers: [AppService, UsersService, TypeUsersService],
 })
 export class AppModule {}
